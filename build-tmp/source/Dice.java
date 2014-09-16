36f480064e97fb0b0114fb16e0ab65c326dbba31
Runjia <runjia.chen44@gmail.com>
2014-09-16 11:26:28 -0700

    Dice Program

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,16 +1,32 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die d1;
 Die d2;
 Die d3;
 Die d4;
 Die d5;
 
-void setup()
+public void setup()
 {
 	size(500,500);
 	noLoop();
 }
 
-void draw()
+public void draw()
 {
 	background(255,255,255);
 
@@ -34,7 +50,7 @@ void draw()
 	d1.addition();//shows the total number of the dots on the dice
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -48,7 +64,7 @@ class Die //models one single dice cube
 		myX=x;
 		myY=y;
 	}
-	void roll()
+	public void roll()
 	{
 		fill(0);
 		if (num==1){
@@ -86,7 +102,7 @@ class Die //models one single dice cube
 		}
 	}
 
-	void show()
+	public void show()
 	{
 		int color1=(int)(Math.random()*255+1);
 		int color2=(int)(Math.random()*255+1);
@@ -95,8 +111,17 @@ class Die //models one single dice cube
 		rect(myX,myY,100,100);	
 	}
 
-	void addition(){
+	public void addition(){
 		textSize(20);
 		text("Total Number:"+(d1.num+d2.num+d3.num+d4.num+d5.num),160,350);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
